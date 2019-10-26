a090842c6895e41f58333132b761752234cf9d3e
HIT_bjm <1161800106@stu.hit.edu.cn>
2019-10-26 16:12:02 +0800

    show viewer

--- real_code/lidar_pose.cpp
+++ real_code/lidar_pose.cpp
@@ -6,6 +6,7 @@
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/point_cloud.h>
+#include <pcl/visualization/pcl_visualizer.h>
 #include <math.h>
 
 struct vele// new lidar data
@@ -37,12 +38,44 @@ int
   pcl::IterativeClosestPoint<vele, vele> icp;
   icp.setInputSource(cloud_out);//out
   icp.setInputTarget(cloud_in);//in
-  icp.setMaximumIterations (300);//number of iterations
+  //icp.setMaximumIterations (300);//number of iterations
   //icp.setMaxCorrespondenceDistance(0.5);
   //icp.setTransformationEpsilon(1e-6);
   // icp.setEuclideanFitnessEpsilon(0.11);
-  pcl::PointCloud<vele> Final;
-  icp.align(Final);
+ // pcl::PointCloud<vele> Final;
+  //icp.align(Final);
+
+  //viewer
+  std::cout << "0" << '\n';
+  pcl::PointCloud<pcl::PointXYZ>::Ptr viewer_out (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr viewer_in (new pcl::PointCloud<pcl::PointXYZ>);
+std::cout << "0.1" << '\n';
+viewer_out->points.resize(cloud_out->points.size());
+viewer_in->points.resize(cloud_in->points.size());
+  for (size_t i = 0; i < cloud_out->points.size(); i++) {
+      viewer_out->points[i].x=cloud_out->points[i].x;
+      viewer_out->points[i].y=cloud_out->points[i].y;
+      viewer_out->points[i].z=cloud_out->points[i].z;
+  }
+  for (size_t i = 0; i < cloud_in->points.size(); i++) {
+      viewer_in->points[i].x=cloud_in->points[i].x;
+      viewer_in->points[i].y=cloud_in->points[i].y;
+      viewer_in->points[i].z=cloud_in->points[i].z;
+  }
+  int v1;
+std::cout << "1" << '\n';
+  pcl::visualization::PCLVisualizer viewer("icp");
+  viewer.setBackgroundColor(0, 0, 0); //创建窗口
+  viewer.createViewPort(0.0, 0.0, 1.0, 1.0, v1);
+  //设置点云颜色
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> ref_color(viewer_in, 0, 255, 0); //投影前可以随便设一个颜色
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> present_color(viewer_out, 255, 255, 255);  //投影后的设置为白色
+  viewer.addPointCloud<pcl::PointXYZ>(viewer_in, ref_color, "ref", v1);
+  viewer.addPointCloud<pcl::PointXYZ>(viewer_out, present_color, "present", v1);
+
+  std::cout << "2" << '\n';
+  viewer.spin();
+std::cout << "3" << '\n';
 
   std::vector<float> angleRPY;//get angle from mat
   angleRPY.resize(3);
